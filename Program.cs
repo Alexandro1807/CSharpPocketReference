6f24b6a5168987f0fa7037adb25113a4a4af90db
Alexander T <sanya.trusov@gmail.com>
2024-05-28 17:20:06 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+Console.WriteLine(MultiplyBy12(30));
+
+int MultiplyBy12(int mult)
+{
+    return mult * 12;
+}
+
+static int Factorial(int x)
+{
+    if (x == 0) return 1;
+    return x * Factorial(x);
+}
+
+
+
+
+
+
+
+
+
+
+//Пауза перед завершением консоли
+Thread.Sleep(3 * 1000);
